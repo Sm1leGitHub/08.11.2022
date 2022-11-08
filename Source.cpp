a0f2bf3298c65741ab12c41c624792f4cd46688d
Sm1leGitHub <113791071+Sm1leGitHub@users.noreply.github.com>
2022-11-08 18:18:05 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "RUS");
+	//1
+	/*int hp;
+	cin >> hp;
+	bool a;
+	a = hp > 0;
+	cout << boolalpha << endl;*/
+	//2
+	/*int a;
+	cin >> a; 
+	if (a > 20) {
+		cout << "on" << endl;
+	}
+	else if (a < 20) {
+		cout << "off" << endl;
+	}*/
+	//4
+		/*double x, y;
+		cin >> x ;
+		if (x > 0) {
+			y = sqrt(x - 2);
+			cout << y << endl;
+		}
+		else if (x == 0) {
+			y == 0;
+			cout << y << endl;
+		}
+		else if (x < 0) {
+			y = abs(x);
+			cout << y << endl;
+		}*/
+	//5
+	/*int t_1,v,t_2;
+	cin >> t_1 >> t_2 >> v;
+	if (t_1 <= v || t_2 > 80) {
+		cout << "on" << endl;
+	}
+	else if (v <= t_1 || t_2 > 80) {
+		cout << "off" << endl;
+	}*/
+	/*int NumberOfFinger = 0;
+
+	cout << "Введите номер: ";
+	cin >> NumberOfFinger;
+
+	case 1:cout << "Результат:  Большой палец ";
+	case 1 (NumberOfFinger == 2)
+		cout << "Результат: Указательный палец";
+	case 1 (NumberOfFinger == 3)
+		cout << "Результат: Средний палец";
+	case 1 (NumberOfFinger == 4)
+		cout << "Результат: Безымянный палец";
+	case 1 (NumberOfFinger == 5)
+		cout << "Результат: Мизинец";
+	else
+		cout << "Нет соответствий!";
+
+	return 0;*/
+}
